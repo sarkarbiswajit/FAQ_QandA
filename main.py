5c3a035f91c056aaabeb82d7c114eb9adc0e4e51
Biswajit Sarkar <101525369+sarkarbiswajit@users.noreply.github.com>
2023-12-14 23:44:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_helper import get_qa_chain, create_vector_db
 
-st.title("FAQ Q&A 🌱")
+st.title("FAQ Q&A ")
 btn = st.button("Create Knowledgebase")
 if btn:
     create_vector_db()
